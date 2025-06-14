00241e6e1f36b4e0f46841eaa13855de287a3060
Yusuke Sakurai <yusuke.sakurai@3-shake.com>
2025-06-14 01:45:33 +0000

    h

--- scripts/runc_integration.sh
+++ scripts/runc_integration.sh
@@ -31,6 +31,7 @@ run.bats:runc run [execve error]
 events.bats:events oom
 events.bats:events --interval default
 rlimits.bats:runc run with RLIMIT_NOFILE(The same as system's hard value)
+mounts.bats:runc run [tmpcopyup]
 mounts.bats:runc run [/proc is a symlink]
 mounts.bats:runc run [ro /sys/fs/cgroup mounts + cgroupns]
 mounts.bats:runc run [mount order, container bind-mount source]
