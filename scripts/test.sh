51c627b0688ff86878989b25a807ffb5b1214141
Yusuke Sakurai <yusuke.sakurai@3-shake.com>
2025-06-20 23:28:28 +0000

    j

--- scripts/test.sh
+++ scripts/test.sh
@@ -31,6 +31,8 @@ for file in $BATS_FILES; do
   mkdir -p "$(dirname "$logfile")"
 
   # sudo -E PATH="$PATH" script -q -e -c "bats -t '$file'" "$logfile"
-  sudo bats -t "$file"
+  BATS_PATH=$(which bats)
+  sudo -E "$BATS_PATH" -t "$file"
+  # sudo bats -t "$file"
   cat $logfile
 done
