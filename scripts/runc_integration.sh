ff2192cf83e65c144503f6d456d6d371797d481a
Yusuke Sakurai <yusuke.sakurai@3-shake.com>
2025-06-20 10:31:41 +0000

    script

--- scripts/runc_integration.sh
+++ scripts/runc_integration.sh
@@ -73,7 +73,7 @@ while IFS= read -r test_case; do
     logfile="./log/$(basename "$test_case").log"
     mkdir -p "$(dirname "$logfile")"
 
-    script -q -e -c "timeout 300s sudo -E PATH=\"$PATH\" \"$BATS_PATH\" \"$test_case\"" > "$logfile" 2>&1
+    sudo script -q -e -c "timeout 300s sudo -E PATH=\"$PATH\" \"$BATS_PATH\" \"$test_case\"" > "$logfile" 2>&1
     exit_code=$?
 
     if [[ $exit_code -eq 124 ]]; then
