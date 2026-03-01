1f824e44e7cfa7cd6d75184e4c08109afb312e7c
Yusuke Sakurai <yusuke.sakurai@3-shake.com>
2026-03-01 06:23:55 +0000

    a

--- tests/contest/contest/src/tests/exec/mod.rs
+++ tests/contest/contest/src/tests/exec/mod.rs
@@ -22,7 +22,7 @@ fn create_spec(process: Option<ProcessBuilder>) -> Result<Spec> {
 
 pub fn get_exec_test() -> TestGroup {
     let mut test_group = TestGroup::new("exec");
-    let preserve_fds_test = Test::new(
+    let _preserve_fds_test = Test::new(
         "preserve_fds_test",
         Box::new(preserve_fds_test::preserve_fds_test),
     );
@@ -31,36 +31,36 @@ pub fn get_exec_test() -> TestGroup {
         Box::new(ignore_paused_test::ignore_paused_test),
     );
     let cgroup_test = Test::new("cgroup_test", Box::new(cgroup_test::cgroup_test));
-    let no_capabilities_test = Test::new(
+    let _no_capabilities_test = Test::new(
         "no_capabilities_test",
         Box::new(cap_test::get_test_no_capabilities),
     );
-    let new_privileges_test = Test::new(
+    let _new_privileges_test = Test::new(
         "new_privileges_test",
         Box::new(cap_test::get_test_new_privileges),
     );
-    let some_capabilities_test = Test::new(
+    let _some_capabilities_test = Test::new(
         "some_capabilities_test",
         Box::new(cap_test::get_test_some_capabilities),
     );
-    let capabilities_by_flag_test_case1 = Test::new(
+    let _capabilities_by_flag_test_case1 = Test::new(
         "capabilities_by_flag_test_case1",
         Box::new(cap_test::get_test_capabilities_by_flag_case1),
     );
-    let capabilities_by_flag_test_case2 = Test::new(
+    let _capabilities_by_flag_test_case2 = Test::new(
         "capabilities_by_flag_test_case2",
         Box::new(cap_test::get_test_capabilities_by_flag_case2),
     );
 
     test_group.add(vec![
-        Box::new(preserve_fds_test),
+        // Box::new(preserve_fds_test),
         // Box::new(ignore_paused_test),
         Box::new(cgroup_test),
-        Box::new(no_capabilities_test),
-        Box::new(new_privileges_test),
-        Box::new(some_capabilities_test),
-        Box::new(capabilities_by_flag_test_case1),
-        Box::new(capabilities_by_flag_test_case2),
+        // Box::new(no_capabilities_test),
+        // Box::new(new_privileges_test),
+        // Box::new(some_capabilities_test),
+        // Box::new(capabilities_by_flag_test_case1),
+        // Box::new(capabilities_by_flag_test_case2),
     ]);
 
     test_group
